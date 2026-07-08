42e6ce52ffa072e81c5e58e41b02d05209553437
Chirag Mahanta <chiragmahanta6@gmail.com>
2026-07-08 10:11:26 +0530

    fixed some errors

--- backend/index.js
+++ backend/index.js
@@ -14,8 +14,10 @@ import cors from 'cors'
 const app = express();
 
 app.use(cors({
-    origin: ENV.CLIENT_URL,
-    credentials: true
+    origin: [ENV.CLIENT_URL, 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(cookieParser())
 app.use(express.json())
